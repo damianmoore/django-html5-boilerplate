bcb57e907209b154b270830ae53721b9b8cc9c81
Damian Moore <damian@epixstudios.co.uk>
2011-09-24 12:14:12 +0100

    Moved Django dependency to setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@
 
 from distutils.core import setup
 
-setup(name='django-html5-boilerplate',
-      version='0.1',
-      description='Scripts for creating and builing Django projects based on HTML5 Boilerplate',
-      author='Damian Moore',
-      author_email='',
-      url='https://github.com/damianmoore/django-html5-boilerplate',
-      packages=['django-html5-boilerplate',],
-      scripts=['django-html5-boilerplate/startproject', 'django-html5-boilerplate/buildproject',]
-      )
+setup(name = 'django-html5-boilerplate',
+    version = '0.1',
+    description = 'Scripts for creating and builing Django projects based on HTML5 Boilerplate',
+    author = 'Damian Moore',
+    author_email = '',
+    url = 'https://github.com/damianmoore/django-html5-boilerplate',
+    packages = ['django-html5-boilerplate',],
+    scripts = ['django-html5-boilerplate/startproject', 'django-html5-boilerplate/buildproject',]
+    install_requires = ['django',]
+)
 
